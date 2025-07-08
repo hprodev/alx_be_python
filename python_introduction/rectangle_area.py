7d9d423ec74847ac8e7afd391a96e9f1b2df87ed
Abdulhakeem Ibrahim <icreate.hakeem@gmail.com>
2025-07-08 10:07:36 +0100

    Area of a rectangle

---
+++ python_introduction/rectangle_area.py
@@ -0,0 +1,6 @@
+length = 10  # (length of a rectangle)
+width = 5  # (width of a rectangle)
+
+area = length * width  # (Formula for Area of a rectangle)
+
+print("The area of the rectangle is: ", area)
