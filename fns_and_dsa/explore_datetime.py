bc90539446e106ce092f0f83ee956f8852dbc5d6
Abdulhakeem Ibrahim <icreate.hakeem@gmail.com>
2025-07-25 08:26:29 +0100

    Datetime

---
+++ fns_and_dsa/explore_datetime.py
@@ -0,0 +1,18 @@
+# The Use of Datetime in Python
+from datetime import datetime
+from datetime import timedelta
+
+
+def display_current_datetime():
+    current_date = datetime.now().replace(microsecond=0)
+    print("Current date and time: ", current_date)
+    return current_date
+
+
+number_of_days = int(input("Enter the number of days to add to the current date: "))
+
+
+def calculate_future_date():
+    future_date = datetime + timedelta(days="number_of_days")
+    print("Future date: ", future_date.date())
+    return future_date
