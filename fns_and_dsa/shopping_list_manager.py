3c5da4368958860cf6f6ab09f8a51d283955448f
Abdulhakeem Ibrahim <icreate.hakeem@gmail.com>
2025-07-25 07:29:03 +0100

    Shopping List Manager

---
+++ fns_and_dsa/shopping_list_manager.py
@@ -0,0 +1,44 @@
+# Shopping List Manager
+
+
+def display_menu():
+    print("Shopping List Manager")
+    print("1. Add Item")
+    print("2. Remove Item")
+    print("3. View List")
+    print("4. Exit")
+
+
+def main():
+    shopping_list = []
+    while True:
+        display_menu()
+        choice = input("Enter your choice: ")
+
+        if choice == "1":
+            # Prompt for and add an item
+            item = input("Input item name: ")
+            shopping_list.append(item)
+            print(f"{item} Added to shopping list")
+        elif choice == "2":
+            # Prompt for and remove an item
+            item = input("Input item name: ")
+            if item in shopping_list:
+                shopping_list.remove(item)
+                print(f"{item} removed from shopping list")
+            else:
+                print(f"{item} not found")
+        elif choice == "3":
+            # Display the shopping list
+            print("Your shopping list:")
+            for item in shopping_list:
+                print(f"- {item}")
+        elif choice == "4":
+            print("Goodbye!")
+            break
+        else:
+            print("Invalid choice. Please try again.")
+
+
+if __name__ == "__main__":
+    main()
